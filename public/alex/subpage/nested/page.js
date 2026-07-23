abc345d00c5a6336e0a79d259479b1b493dc6f68
Aleks Miller <eskandarme@gmail.com>
2026-07-23 12:24:37 +0300

    feat(framework): migrate alexs pages

--- public/alex/subpage/nested/page.js
+++ public/alex/subpage/nested/page.js
@@ -1,28 +1,33 @@
-import { a, h2, h3, p } from "/app.js";
+import app, { a, h2, h3, p, Page } from "/app.js";
 
-app.$body.ac("theme-1");
+export default new class NestedPage extends Page {
+	title = "Nested subpage";
+	href = "/alex/subpage/nested/";
 
-export default {
-  href: "/alex/subpage/nested/",
-  link() {
-    return a.c("page-link", this.href).href("/alex/subpage/nested/");
-  },
-  preview() {
-    a.c("page-link-block", () => {
-      h2("Preview");
-      p("Preview text");
-    }).href(this.href);
-  },
-  content() {
-    h3("Alex's subpage");
-    p("Nested subpage content");
-  },
-  full() {
-    a.c("page-back", "Back").href("../");
-    this.preview();
-    this.content();
-  },
-  render() {
-    this.full();
-  },
-};
+	link() {
+		return a.c("page-link", this.href).href(this.href);
+	}
+
+	preview() {
+		a.c("page-link-block", () => {
+			h2("Preview");
+			p("Preview text");
+		}).href(this.href);
+	}
+
+	content() {
+		h3("Alex's subpage");
+		p("Nested subpage content");
+	}
+
+	full() {
+		a.c("page-back", "Back").href("../");
+		this.preview();
+		this.content();
+	}
+
+	render() {
+		app.$body.ac("theme-1");
+		this.full();
+	}
+}
